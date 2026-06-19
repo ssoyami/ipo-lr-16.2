f4ceb888e6af9e599d1c2bd4a593200033514ed5
ssoya <rfrfrfrfley@gmail.com>
2026-06-19 23:15:24 +0300

    Update admin.py

--- myshop/store/admin.py
+++ myshop/store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (
+    Category,
+    Manufacturer,
+    Product,
+    Cart,
+    CartItem,
+)
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Manufacturer)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItem)
